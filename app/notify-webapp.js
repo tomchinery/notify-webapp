540f0546bce087228ce70ec77933097a22891576
Tom Chinery <hello@tomchinery.com>
2016-02-20 21:39:55 +0000

    Add notifications and list them

--- app/notify-webapp.js
+++ app/notify-webapp.js
@@ -5,9 +5,40 @@ if (Meteor.isClient) {
 
   angular.module('notifyapp', ['angular-meteor'])
 
-  angular.module('notifyapp').controller('notificationsListController', ['$meteor',
-    function ($meteor) {
-      this.notifications = $meteor.collection(Notifications)
+  angular.module('notifyapp').controller('notificationsListController', ['$meteor', '$scope',
+    function ($meteor, $scope) {
+      $scope.notifications = $meteor.collection(Notifications)
+    }
+  ])
+
+  angular.module('notifyapp').directive('addNotification',
+    function () {
+      return {
+        restrict: 'E',
+        templateUrl: 'views/add-notification.html',
+        controller: 'addNotificationController',
+      }
+    }
+  )
+
+  angular.module('notifyapp').controller('addNotificationController', ['$meteor', '$scope',
+    function ($meteor, $scope) {
+      $scope.notifications = $meteor.collection(Notifications)
+      $scope.add = false
+
+      $scope.$on('addNotification', function () {
+        console.log('YEN1')
+        $scope.add = true
+      })
+
+      $scope.addNotification = function (newNotification) {
+        $scope.notifications.push({
+          title: newNotification.title,
+          body: newNotification.body,
+          createdAt: new Date()
+        })
+        console.log($scope.notifications)
+      }
     }
   ])
 

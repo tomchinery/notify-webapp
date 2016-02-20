7aeb9c66cd46f42e0d511af4afdd03c22c1fb6f7
Tom Chinery <hello@tomchinery.com>
2016-02-20 10:45:29 +0000

    Add: App body to markup

--- app/notify-webapp.js
+++ app/notify-webapp.js
@@ -1,6 +1,15 @@
+// define collections
+Notifications = new Mongo.Collection('notifications')
+
 if (Meteor.isClient) {
 
   angular.module('notifyapp', ['angular-meteor'])
+
+  angular.module('notifyapp').controller('notificationsListController', ['$meteor',
+    function ($meteor) {
+      this.notifications = $meteor.collection(Notifications)
+    }
+  ])
 
 }
 

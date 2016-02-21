8349e707751f8c56737186aecab4a52c2a749e22
Tom Chinery <hello@tomchinery.com>
2016-02-21 22:25:39 +0000

    Test build

---
+++ build/programs/server/packages/global-imports.js
@@ -0,0 +1,18 @@
+/* Imports for global scope */
+
+MongoInternals = Package.mongo.MongoInternals;
+Mongo = Package.mongo.Mongo;
+Tracker = Package.tracker.Tracker;
+Deps = Package.tracker.Deps;
+Date = Package['es5-shim'].Date;
+parseInt = Package['es5-shim'].parseInt;
+Meteor = Package.meteor.Meteor;
+WebApp = Package.webapp.WebApp;
+main = Package.webapp.main;
+WebAppInternals = Package.webapp.WebAppInternals;
+_ = Package.underscore._;
+DDP = Package['ddp-client'].DDP;
+DDPServer = Package['ddp-server'].DDPServer;
+LaunchScreen = Package['launch-screen'].LaunchScreen;
+Autoupdate = Package.autoupdate.Autoupdate;
+
